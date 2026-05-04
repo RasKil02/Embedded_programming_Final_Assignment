547cb26233845a9184abbd8b72a1844c668205a0
Rasmus <rasmus_kildeberg@outlook.dk>
2026-05-04 19:54:12 +0200

    Keypad

--- src/main.c
+++ src/main.c
@@ -12,12 +12,15 @@
 #include "status_led.h"
 #include "leds.h"
 #include "adc.h"
+#include "Keypad.h"
 
 #define USERTASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define IDLE_PRIO 0
 #define LOW_PRIO  1
 #define MED_PRIO  2
 #define HIGH_PRIO 3
+
+QueueHandle_t key_queue;
 
 static void setupHardware(void)
 /*****************************************************************************
@@ -37,6 +40,11 @@ static void setupHardware(void)
 int main(void)
 {
     setupHardware();
+
+    key_queue =  xQueueCreate( 10, sizeof( INT8U ) ); // Is this correct?
+
+
+    xTaskCreate( key_task, "Keyboard_task", USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
     xTaskCreate( status_led_task, "Status_led", USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
     xTaskCreate( red_led_task,    "Red_led",    USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
     xTaskCreate( yellow_led_task, "Yellow_led", USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
